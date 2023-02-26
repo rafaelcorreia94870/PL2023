d0bc5ae7341bbcaeae423bd8dabfe1957dafa16e
rafaelcorreia94870 <a94870@alunos.uminho.pt>
2023-02-26 16:58:33 +0000

    commit bue da fixe

---
+++ TPC2/tpc2.py
@@ -0,0 +1,43 @@
+def findsword(string, word):
+    r=string.find(word)
+    if(r==-1):
+        return False
+    else:
+        return True
+    
+def findsnumbers(inpt):
+    numbers=[]
+    r = 0
+    currentnumber=''
+    for c in inpt:
+        if c.isdigit():
+            currentnumber+=c
+        else:
+            if not currentnumber=='':
+                numbers.append(int(currentnumber))
+                currentnumber=''
+    if not currentnumber=='':
+                numbers.append(int(currentnumber))
+    for x in numbers:
+        r+=x
+    return r
+
+def main():
+    sum=0
+    flag=True
+    while(True):
+        inpt=input()
+        inpt=inpt.lower()
+        if(findsword(inpt,"on")):
+            flag=True
+        if(findsword(inpt,"off")):
+            sum=0
+            flag=False
+        if(flag and findsword(inpt,"=")):
+            print(f"O Resultado da soma é: {sum}")
+        elif flag:
+            r = findsnumbers(inpt)
+            sum+=r
+
+if __name__ == '__main__':
+    main()
